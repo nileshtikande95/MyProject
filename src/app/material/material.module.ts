2add1ed103febc91741174675759c2756141db69
Nilesh Tikande <nilesh.tikande73@gmail.com>
2020-08-01 16:24:35 +0530

    Save Button

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import * as  Material from '@angular/material';
+@NgModule({
+  imports: [
+    CommonModule,
+    Material.MatToolbarModule,
+    Material.MatButtonModule,
+    Material.MatGridListModule,
+    Material.MatFormFieldModule,
+    Material.MatInputModule,
+    Material.MatRadioModule,
+    Material.MatSelectModule,
+    Material.MatDatepickerModule,
+    Material.MatNativeDateModule,
+    Material.MatCheckboxModule,
+    Material.MatTableModule,
+    Material.MatPaginatorModule,
+    Material.MatSortModule,
+    Material.MatDialogModule,
+    Material.MatListModule,
+    Material.MatSidenavModule,
+    Material.MatCheckboxModule,
+    Material.MatCardModule,
+    Material.MatTabsModule,
+    Material.MatIconModule,
+    Material.MatCardModule,
+    Material.MatTabsModule
+  ],
+  exports: [
+    Material.MatToolbarModule,
+    Material.MatButtonModule,
+    Material.MatGridListModule,
+    Material.MatFormFieldModule,
+    Material.MatInputModule,
+    Material.MatRadioModule,
+    Material.MatSelectModule,
+    Material.MatDatepickerModule,
+    Material.MatNativeDateModule,
+    Material.MatCheckboxModule,
+    Material.MatTableModule,
+    Material.MatPaginatorModule,
+    Material.MatSortModule,
+    Material.MatDialogModule,
+    Material.MatListModule,
+    Material.MatSidenavModule,
+    Material.MatCheckboxModule,
+    Material.MatIconModule,
+    Material.MatCardModule,
+    Material.MatTabsModule
+
+  ],
+  declarations: []
+})
+export class MaterialModule { }
